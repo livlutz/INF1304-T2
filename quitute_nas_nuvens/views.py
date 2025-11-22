b0d1e97d37662c85dd309ea54570b1403c30b3fb
Lívia Lutz <108961867+livlutz@users.noreply.github.com>
2025-11-22 22:32:19 +0000

    ajustando html

--- quitute_nas_nuvens/views.py
+++ quitute_nas_nuvens/views.py
@@ -4,22 +4,23 @@ Views principais do projeto Quitute nas Nuvens.
 
 from django.shortcuts import render, redirect
 from django.views import View
+from consumidor.models import Item
 
 
 class HomepageView(View):
     """
     View da página inicial que captura o e-mail do comprador.
-    
+
     GET: Exibe o formulário para entrada de e-mail
     POST: Armazena o e-mail na sessão e redireciona para lista de quitutes
     """
-    
+
     template_name = 'items/homepage.html'
-    
+
     def get(self, request):
         """Renderiza a página inicial."""
         return render(request, self.template_name)
-    
+
     def post(self, request):
         """Processa o formulário de e-mail e armazena na sessão."""
         email = request.POST.get('email')
@@ -27,3 +28,4 @@ class HomepageView(View):
             request.session['customer_email'] = email
             return redirect('item_list')
         return render(request, self.template_name)
+
