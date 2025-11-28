e5ecd008079b5b25fc841a7ac0b8853902bea8e3
Lívia Lutz <108961867+livlutz@users.noreply.github.com>
2025-11-28 01:45:53 +0000

    configuaracoes de imagens + imagens dos produtos

--- consumidor/models.py
+++ consumidor/models.py
@@ -3,7 +3,8 @@ from django.db import models
 class Item(models.Model):
     nome = models.CharField(max_length=200)
     estoque = models.IntegerField(default=0)
-    
+    imagem = models.ImageField(upload_to='produtos/', null=True, blank=True)
+
     def __str__(self):
         return self.nome
 
@@ -13,7 +14,7 @@ class Reserva(models.Model):
     email_cliente = models.EmailField()
     quantidade = models.IntegerField()
     data_reserva = models.DateTimeField(auto_now_add=True)
-    
+
     def __str__(self):
         return f"{self.nome_cliente} - {self.item.nome} ({self.quantidade})"
 
